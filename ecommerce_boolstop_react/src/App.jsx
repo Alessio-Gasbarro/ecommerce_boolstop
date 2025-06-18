5fd3d76cfe02fbe77c3091904c185714e3922d9c
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-06-18 17:38:17 +0200

    [UPD] creazione pagina che contiene tutti i videogames

--- ecommerce_boolstop_react/src/App.jsx
+++ ecommerce_boolstop_react/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
+import Videogames from './pages/Videogames';
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/all" element={<Videogames />} />
       </Routes>
     </BrowserRouter>
   );
