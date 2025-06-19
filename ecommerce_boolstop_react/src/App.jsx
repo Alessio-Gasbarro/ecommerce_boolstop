09e80879d785392dddb3304e72f67dfa699405e2
danielemurgioni <dashomew@gmail.com>
2025-06-19 09:33:29 +0200

    [UPD] Created BasketPage

--- ecommerce_boolstop_react/src/App.jsx
+++ ecommerce_boolstop_react/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Basket from './pages/Basket'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/basket" element={<Basket />} />
       </Routes>
     </BrowserRouter>
   );
