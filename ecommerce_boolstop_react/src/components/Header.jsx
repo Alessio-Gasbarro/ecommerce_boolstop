d652aae5cf1b5e96d4c5db8201a0a84891c28770
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-06-19 22:39:58 +0200

    [FIX] Added small CSS Fixes

--- ecommerce_boolstop_react/src/components/Header.jsx
+++ ecommerce_boolstop_react/src/components/Header.jsx
@@ -6,11 +6,9 @@ export default function Header() {
     return (
         <div className="header-wrapper">
             <header className="header">
-                <Link to="/">
-                    <div className="logo">
-                        <img src={logoBoolStop} alt="Logo BoolStop" />
-                        <h1>BoolStop</h1>
-                    </div>
+                <Link to="/" className="logo">
+                    <img src={logoBoolStop} alt="Logo BoolStop" />
+                    <span className="logo-text">BoolStop</span>
                 </Link>
                 <nav className="icons">
                     <input
@@ -21,6 +19,6 @@ export default function Header() {
                     <Link to="/basket"><i className="fas fa-shopping-cart"></i></Link>
                 </nav>
             </header>
-        </div>
+        </div >
     );
 }
