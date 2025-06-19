f121734ffb28b2e28ffed589e4d66c895b264a57
danielemurgioni <dashomew@gmail.com>
2025-06-19 09:40:54 +0200

    [FIX]

--- ecommerce_boolstop_react/src/App.jsx
+++ ecommerce_boolstop_react/src/App.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-<<<<<<< HEAD
 import Videogames from './pages/Videogames';
-=======
 import Basket from './pages/Basket'
 
->>>>>>> create/basket-page
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
         <Route path="/all" element={<Videogames />} />
-=======
         <Route path="/basket" element={<Basket />} />
->>>>>>> create/basket-page
       </Routes>
     </BrowserRouter>
   );
